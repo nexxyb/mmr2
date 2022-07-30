6a36f82b15a82b87fc90371a5d31abb2e61530e4
Wasiu <nexxyb@gmail.com>
2022-07-30 12:40:17 +0100

    Edited for deployment

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn recom.wsgi:application --log-file - --log-level debug
+web: gunicorn deployt.wsgi:application --log-file - --log-level debug
 release: python manage.py migrate
--- deployt/urls.py
+++ deployt/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf.urls.static import static
-fro
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('recommender.urls')),
