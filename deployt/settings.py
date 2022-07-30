b85589ef4de6ccb78c6d497eaf853e78dc184aa0
Wasiu <nexxyb@gmail.com>
2022-07-30 12:55:59 +0100

    Debug set to false

--- deployt/settings.py
+++ deployt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
 ALLOWED_HOSTS = [
     'guarded-tundra-24771.herokuapp.com',
